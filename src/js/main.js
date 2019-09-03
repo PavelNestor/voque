7c9eb7a34201276708975a5b24847c024274005c
PavelNestor <pavelnesterets82@gmail.com>
2019-09-03 07:58:10 +0300

    add new text effect

--- src/js/main.js
+++ src/js/main.js
@@ -25,15 +25,25 @@ window.addEventListener("scroll", function() {
 
   //parallax
   var textParallax = document.getElementsByClassName("text-parallax");
-  for (let index = 0; index < textParallax.length; index++) {
-    const text = textParallax[index];
+  var boxParallax = document.getElementsByClassName("box-parallax");
+  for (let index = 0; index < boxParallax.length; index++) {
+    const box = boxParallax[index];
+    const top = box.getBoundingClientRect().top;
 
-    if(index == 2 || index == 3 || index == 6 || index == 7) {
-      text.style.transform =
-        "scale(-1, -1) translate3d(0, " + calculateTranslateBottom(pageYOffset, index) + "px, 0)";
-    } else {
-      text.style.transform =
-        "scale(-1, -1) translate3d(0, " + calculateTranslateTop(pageYOffset, index) + "px, 0)";
+    if (top < 200 && top > -900) {
+      for (let index = 0; index < textParallax.length; index++) {
+        const text = textParallax[index];
+        console.log('top', top);
+        
+        if(index == 2 || index == 3 || index == 6 || index == 7) {
+          text.style.transform =
+            "scale(-1, -1) translate3d(0, " + (top * 0.1) + "px, 0)";
+        } else {
+          text.style.transform =
+            "scale(-1, -1) translate3d(0, " + (- top * 0.1) + "px, 0)";
+        }
+      }
+      
     }
   }
 
