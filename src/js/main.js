5cfaec74781e4728aa83e4a507598af9ba97ade9
PavelNestor <pavelnesterets82@gmail.com>
2019-09-04 16:14:13 +0300

    some polish

--- src/js/main.js
+++ src/js/main.js
@@ -20,114 +20,242 @@ handlerToogleMenu();
 var logo = document.getElementById("logo");
 var logoWrapper = document.getElementById("logo-wrapper");
 var menuImg = document.getElementById("menu-image");
-var sections = document.getElementsByTagName("section");
+var sections = Array.from(document.getElementsByTagName("section"));
 var linesMenuItems = document.getElementsByClassName("lines-menu__item");
 
+let currentSection = "";
+let lastSection = "";
+let windowHeight = 0;
+let breakpoints = [];
+let lastScrollPosition = 0;
+
+const sectionOptions = [
+  {
+    id: "hero",
+    logoUrl: "img/logo.svg",
+    navbarClass: "hero",
+    scrollClass: "hero",
+    isInvert: false,
+    isLogoVisible: false,
+  },
+  {
+    id: "about",
+    logoUrl: "img/logo-black.svg",
+    navbarClass: "about",
+    contentId: "about-content",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: true
+  },
+  {
+    id: "services",
+    logoUrl: "img/logo.svg",
+    navbarClass: "about",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: false
+  },
+  {
+    id: "development",
+    logoUrl: "img/logo.svg",
+    navbarClass: "about",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: false
+  },
+  {
+    id: "outsource",
+    logoUrl: "img/logo.svg",
+    navbarClass: "about",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: false
+  },
+  {
+    id: "outstaffing",
+    logoUrl: "img/logo.svg",
+    navbarClass: "about",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: false
+  },
+  {
+    id: "contact",
+    logoUrl: "img/logo.svg",
+    navbarClass: "about",
+    scrollClass: "about",
+    isLogoVisible: true,
+    isInvert: false
+  }
+];
+
+function onResize() {
+  breakpoints = [];
+  windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const offset = Math.ceil(windowHeight / 2);
+
+  sections.forEach(section => {
+    breakpoints.push({
+      id: section.id,
+      breakpointTop: section.offsetTop - offset,
+      breakpointBottom: section.offsetTop + section.offsetHeight
+    });
+  });
+}
+
+onResize();
+
+window.addEventListener("resize", onResize);
 
 window.addEventListener("scroll", function() {
+  if (document.body.getBoundingClientRect().top > lastScrollPosition) {
+    // UP SCROLL
 
-  //lines-menu-active
-  for (let index = 0; index < sections.length; index++) {
-    const section = sections[index];
-    const sectionTop = section.getBoundingClientRect().top;
+    logoWrapper.style.top = "0";
 
-    if (sectionTop < 0 && sectionTop > -150) {
-      const sectionId = section.getAttribute("data-id");
-      const logoUrl = section.getAttribute("data-logo");
-      const isVisible = section.getAttribute("data-logo-visibility");
-      const isInvert = section.hasAttribute("data-invert");
+    currentSection = findCurrentSection(pageYOffset, breakpoints);
 
-      console.log('isInvert', isInvert);
+    if (lastSection !== currentSection) {
+      let current = sectionOptions.find(
+        sectionOpt => sectionOpt.id === currentSection
+      );
 
-      invert(linesMenuItems, isInvert);
-      invert(menuImg, isInvert);
+      // document.getElementById(current.contentId).classList.remove('hiddenContent');
 
-      for (let InnerIndex = 0; InnerIndex < linesMenuItems.length; InnerIndex++) {
-        const linesMenuItem = linesMenuItems[InnerIndex];
-        const linkId = linesMenuItem.getAttribute("data-link");
+      invert(linesMenuItems, current.isInvert);
+      invert(menuImg, current.isInvert);
 
-        if(linkId === sectionId) {
-          linesMenuItem.classList.add("lines-menu__item-active");
-        } else {
-          linesMenuItem.classList.remove("lines-menu__item-active");
-        }
-      }
+      setActiveLink(current);
 
-      if (isVisible) {
-        logo.src = logoUrl;
+      if (current.isLogoVisible) {
+        logo.src = current.logoUrl;
         logoWrapper.classList.add("logo-visible");
       } else {
         logoWrapper.classList.remove("logo-visible");
       }
 
-    };
-  }
+    }
 
-  //parallax
-  var textParallax = document.getElementsByClassName("text-parallax");
-  var boxParallax = document.getElementsByClassName("box-parallax");
-  for (let index = 0; index < boxParallax.length; index++) {
-    const box = boxParallax[index];
-    const top = box.getBoundingClientRect().top;
+    lastSection = currentSection;
+  } else {
+    // DOWN SCROLL
 
-    if (top < 200 && top > -900) {
-      for (let index = 0; index < textParallax.length; index++) {
-        const text = textParallax[index];
-        if(index == 0 || index == 1 || index == 4 || index == 5 || index == 8 || index == 9) {
-          text.style.transform =
-            "scale(-1, -1) translate3d(0, " + (pageYOffset * 0.1) + "px, 0)";
-        } else {
-          text.style.transform =
-            "scale(-1, -1) translate3d(0, " + (pageYOffset * - 0.1) + "px, 0)";
-        }
+    logoWrapper.style.top = "-6rem";
+
+    currentSection = findCurrentSection(pageYOffset, breakpoints);
+
+    if (lastSection !== currentSection) {
+      let current = sectionOptions.find(
+        sectionOpt => sectionOpt.id === currentSection
+      );
+
+      invert(linesMenuItems, current.isInvert);
+      invert(menuImg, current.isInvert);
+
+      setActiveLink(current);
+
+      if (current.isLogoVisible) {
+        logo.src = current.logoUrl;
+        logoWrapper.classList.add("logo-visible");
+      } else {
+        logoWrapper.classList.remove("logo-visible");
       }
-      
+
     }
+
+    lastSection = currentSection;
   }
 
-  //parallax mobile
-  var textParallaxMobile = document.getElementsByClassName(
-    "text-parallax-mobile"
-  );
-  for (let index = 0; index < textParallaxMobile.length; index++) {
-    const text = textParallaxMobile[index];
+  lastScrollPosition = document.body.getBoundingClientRect().top;
 
-    if(index == 2 || index == 3 || index == 6 || index == 7) {
-      text.style.transform =
-        "scale(-1, -1) translate3d(0, " + calculateTranslateTop(pageYOffset, index) + "px, 0)";
-    } else {
-      text.style.transform =
-        "scale(-1, -1) translate3d(0, " + calculateTranslateBottom(pageYOffset, index) + "px, 0)";
-    }
-  }
+  // //parallax
+  // var textParallax = document.getElementsByClassName("text-parallax");
+  // var boxParallax = document.getElementsByClassName("box-parallax");
+  // for (let index = 0; index < boxParallax.length; index++) {
+  //   const box = boxParallax[index];
+  //   const top = box.getBoundingClientRect().top;
 
-  // logo change
-  // for (var index = 0; index < sections.length; index++) {
-  //   var section = sections[index];
-  //   if (pageYOffset > section.offsetTop - 34) {
-      
+  //   if (top < 200 && top > -900) {
+  //     for (let index = 0; index < textParallax.length; index++) {
+  //       const text = textParallax[index];
+  //       if (
+  //         index == 0 ||
+  //         index == 1 ||
+  //         index == 4 ||
+  //         index == 5 ||
+  //         index == 8 ||
+  //         index == 9
+  //       ) {
+  //         text.style.transform =
+  //           "scale(-1, -1) translate3d(0, " + pageYOffset * 0.1 + "px, 0)";
+  //       } else {
+  //         text.style.transform =
+  //           "scale(-1, -1) translate3d(0, " + pageYOffset * -0.1 + "px, 0)";
+  //       }
+  //     }
+  //   }
+  // }
+
+  // //parallax mobile
+  // var textParallaxMobile = document.getElementsByClassName(
+  //   "text-parallax-mobile"
+  // );
+
+  // for (let index = 0; index < textParallaxMobile.length; index++) {
+  //   const text = textParallaxMobile[index];
+
+  //   if (index == 2 || index == 3 || index == 6 || index == 7) {
+  //     text.style.transform =
+  //       "scale(-1, -1) translate3d(0, " + calculateTranslateTop(pageYOffset, index) + "px, 0)";
+  //   } else {
+  //     text.style.transform =
+  //       "scale(-1, -1) translate3d(0, " + calculateTranslateBottom(pageYOffset, index) + "px, 0)";
   //   }
   // }
 });
+
+function setActiveLink(current) {
+  for (let index = 0; index < linesMenuItems.length; index++) {
+    const linesMenuItem = linesMenuItems[index];
+    const linkId = linesMenuItem.dataset.link;
+    if (linkId === current.id) {
+      linesMenuItem.classList.add("lines-menu__item-active");
+    } else {
+      linesMenuItem.classList.remove("lines-menu__item-active");
+    }
+  }
+}
+
+function findCurrentSection(pageYOffset, breakpoints) {
+  for (let index = 0; index < breakpoints.length; index++) {
+    const breakpoint = breakpoints[index];
+    if (
+      pageYOffset > breakpoint.breakpointTop &&
+      breakpoints[index + 1] ? pageYOffset < breakpoints[index + 1].breakpointTop : true
+    ) {
+      return breakpoint.id;
+    }
+  }
+}
 
 function invert(items, isInvert) {
   if (isInvert) {
     if (items.length) {
       for (let index = 0; index < items.length; index++) {
         const item = items[index];
-        item.classList.add('invert');
+        item.classList.add("invert");
       }
     } else {
-      items.classList.add('invert');
+      items.classList.add("invert");
     }
-  }else{
+  } else {
     if (items.length) {
       for (let index = 0; index < items.length; index++) {
         const item = items[index];
-        item.classList.remove('invert');
+        item.classList.remove("invert");
       }
     } else {
-      items.classList.remove('invert');
+      items.classList.remove("invert");
     }
   }
   return;
@@ -141,50 +269,62 @@ function calculateTranslateTop(pageYOffset, index) {
 
 function calculateTranslateBottom(pageYOffset, index) {
   return (
-    pageYOffset * 0.1 - ((index + 1) % 2 !== 0 ? (index + 1) * 100 : index * 100)
+    pageYOffset * 0.1 -
+    ((index + 1) % 2 !== 0 ? (index + 1) * 100 : index * 100)
   );
 }
 
-
 // form validation
-var form  = document.getElementsByTagName('form')[0];
-var email = document.getElementById('email');
-var formName = document.getElementById('name');
-var errorEmail = document.querySelector('.form-email-error');
-var errorName = document.querySelector('.form-name-error');
+var form = document.getElementsByTagName("form")[0];
+var email = document.getElementById("email");
+var formName = document.getElementById("name");
+var errorEmail = document.querySelector(".form-email-error");
+var errorName = document.querySelector(".form-name-error");
 
-email.addEventListener("input", function (event) {
-  errorEmail.innerHTML = "";
-  errorEmail.className = "form-email-error";
-}, false);
-
-formName.addEventListener("input", function (event) {
-  errorName.innerHTML = "";
-  errorName.className = "form-name-error";
-}, false);
-
-form.addEventListener("submit", function (event) {
-  var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var isEmail = email.value.length !== 0 && emailRegExp.test(email.value);
-  var isName = formName.value.length === 0;
-
-  if (!isEmail) {
-    errorEmail.innerHTML = "error";
-    errorEmail.className = "form-email-error form-error_active";
-    event.preventDefault();
-    return false;
-  } else {
+email.addEventListener(
+  "input",
+  function(event) {
     errorEmail.innerHTML = "";
     errorEmail.className = "form-email-error";
-  }
-    debugger
-  if (isName) {
-    errorName.innerHTML = "error";
-    errorName.className = "form-name-error form-error_active";
-    event.preventDefault();
-    return false;
-  } else {
+  },
+  false
+);
+
+formName.addEventListener(
+  "input",
+  function(event) {
     errorName.innerHTML = "";
     errorName.className = "form-name-error";
-  }
-}, false);
+  },
+  false
+);
+
+form.addEventListener(
+  "submit",
+  function(event) {
+    var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var isEmail = email.value.length !== 0 && emailRegExp.test(email.value);
+    var isName = formName.value.length === 0;
+
+    if (!isEmail) {
+      errorEmail.innerHTML = "error";
+      errorEmail.className = "form-email-error form-error_active";
+      event.preventDefault();
+      return false;
+    } else {
+      errorEmail.innerHTML = "";
+      errorEmail.className = "form-email-error";
+    }
+    debugger;
+    if (isName) {
+      errorName.innerHTML = "error";
+      errorName.className = "form-name-error form-error_active";
+      event.preventDefault();
+      return false;
+    } else {
+      errorName.innerHTML = "";
+      errorName.className = "form-name-error";
+    }
+  },
+  false
+);
